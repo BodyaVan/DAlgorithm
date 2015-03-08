e756dd903b33db01ca5f203311e15adccaf535b4
BodyaVan <bodya.van@gmail.com>
2015-03-08 17:58:05 +0300

    Add tests for 'and' and 'or'.

--- src/test/java/ua/kpi/dalgorithm/SignalMathTest.java
+++ src/test/java/ua/kpi/dalgorithm/SignalMathTest.java
@@ -6,21 +6,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static ua.kpi.dalgorithm.Signal.*;
 import static ua.kpi.dalgorithm.SignalMath.and;
+import static ua.kpi.dalgorithm.SignalMath.or;
 
 public class SignalMathTest {
-
     @Test
     public void and_0_1() throws Exception {
         assertThat(and(ZERO, ONE), is(ZERO));
     }
 
     @Test
-    public void or() throws Exception {
-
+    public void or_0_1() throws Exception {
+        assertThat(or(ZERO, ONE), is(ONE));
     }
 
     @Test
-    public void not() throws Exception {
-
+    public void not_0() throws Exception {
+        assertThat(not(ZERO), is(ONE));
     }
 }
