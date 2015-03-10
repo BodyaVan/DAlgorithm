d8be2024003fb8415628e1bf9dd4d653828fb805
BodyaVan <bodya.van@gmail.com>
2015-03-10 22:49:48 +0300

    Create the util class with auxiliary methods for creating lists.

---
+++ src/main/java/ua/kpi/dalgorithm/util/Util.java
@@ -0,0 +1,45 @@
+package ua.kpi.dalgorithm.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Created on 10.03.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+public final class Util {
+    private Util() {
+    }
+
+    public static <E> List<E> createConstructedList(int size, Supplier<List<E>> listFactory, Supplier<E> itemFactory) {
+        List<E> resultList = listFactory.get();
+
+        for (int i = 0; i < size; i++) {
+            E item = itemFactory.get();
+            resultList.add(item);
+        }
+
+        return resultList;
+    }
+
+    public static <E> List<E> createConstructedList(int size, Supplier<E> itemFactory) {
+        return createConstructedList(size, ArrayList::new, itemFactory);
+    }
+
+    public static <E> List<E> createFilledList(int size, Supplier<List<E>> listFactory, E filler) {
+        List<E> resultList = listFactory.get();
+
+        for (int i = 0; i < size; i++) {
+            resultList.add(filler);
+        }
+
+        return resultList;
+    }
+
+    public static <E> List<E> createFilledList(int size, E filler) {
+        return createFilledList(size, ArrayList::new, filler);
+    }
+}
---
+++ src/test/java/ua/kpi/dalgorithm/util/UtilTest.java
@@ -0,0 +1,44 @@
+package ua.kpi.dalgorithm.util;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static ua.kpi.dalgorithm.util.Util.createConstructedList;
+import static ua.kpi.dalgorithm.util.Util.createFilledList;
+
+public class UtilTest {
+    @Test
+    public void crateConstructedStringArrayList_3argsMethod() throws Exception {
+        List<String> list = createConstructedList(5, ArrayList::new, String::new);
+
+        assertThat(list, hasItems("", "", "", "", ""));
+    }
+
+    @Test
+    public void crateConstructedStringArrayList_2argsMethod() throws Exception {
+        List<String> list = Util.createConstructedList(5, String::new);
+
+        assertThat(list, hasItems("", "", "", "", ""));
+    }
+
+    //--------------------------------------------------
+
+    @Test
+    public void createFilledIntegerArrayList_3argsMethod() throws Exception {
+        List<Integer> list = createFilledList(5, ArrayList::new, Integer.valueOf(1));
+
+        assertThat(list, hasItems(1, 1, 1, 1));
+    }
+
+    @Test
+    public void createFilledNullArrayList_3argsMethod() throws Exception {
+        List<Integer> list = createFilledList(5, ArrayList::new, null);
+
+        assertThat(list, hasItems(nullValue(), nullValue(), nullValue(), nullValue(), nullValue()));
+    }
+}
