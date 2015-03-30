0bea40bc983760be6fa97fffbc28809df67e7adc
BodyaVan <bodya.van@gmail.com>
2015-03-30 20:18:43 +0300

    add javadoc on getUnimporatantInput() method in LogicOperation

--- src/main/java/ua/kpi/dalgorithm/logic_op/LogicOperation.java
+++ src/main/java/ua/kpi/dalgorithm/logic_op/LogicOperation.java
@@ -11,5 +11,10 @@ public interface LogicOperation {
     int execute(int... inputSignals);
     Signal execute(Signal... inputSignals);
 
+    /**
+     * Returns input signal that has no influence on result
+     *
+     * @return unimportant input signal
+     */
     Signal getUnimportantInput();
 }
