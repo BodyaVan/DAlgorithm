3648e98592d19a73fb5ae5f5853e546bd42231b5
BodyaVan <bodya.van@gmail.com>
2015-04-01 03:35:21 +0300

    create LogicElementsFactory

---
+++ src/main/java/ua/kpi/dalgorithm/logic_elem/LogicElementsFactory.java
@@ -0,0 +1,46 @@
+package ua.kpi.dalgorithm.logic_elem;
+
+import ua.kpi.dalgorithm.logic_op.LogicOperation;
+import ua.kpi.dalgorithm.logic_op.LogicOperations;
+
+/**
+ * Created on 30.03.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+public final class LogicElementsFactory {
+    private LogicElementsFactory() {
+    }
+
+    public static LogicElement createAndLogicElement() {
+        return new LogicElement(LogicOperations.AND);
+    }
+
+    public static LogicElement createOrLogicElement() {
+        return new LogicElement(LogicOperations.OR);
+    }
+
+    public static LogicElement createNotAndLogicElement() {
+        return new LogicElement(LogicOperations.NOT_AND);
+    }
+
+    public static LogicElement createNotOrLogicElement() {
+        return new LogicElement(LogicOperations.NOT_OR);
+    }
+
+    public static LogicElement createAndLogicElement(int inputsQuantity) {
+        return new LogicElement(LogicOperations.AND, inputsQuantity);
+    }
+
+    public static LogicElement createOrLogicElement(int inputsQuantity) {
+        return new LogicElement(LogicOperations.OR, inputsQuantity);
+    }
+
+    public static LogicElement createNotAndLogicElement(int inputsQuantity) {
+        return new LogicElement(LogicOperations.NOT_AND, inputsQuantity);
+    }
+
+    public static LogicElement createNotOrLogicElement(int inputsQuantity) {
+        return new LogicElement(LogicOperations.NOT_OR, inputsQuantity);
+    }
+}
