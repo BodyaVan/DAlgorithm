cb7c9455cf09150820a22aebb786da1f0edb1419
BodyaVan <bodya.van@gmail.com>
2015-04-05 18:05:06 +0300

    add interfaces Nameable, Identifiable

---
+++ src/main/java/ua/kpi/dalgorithm/logic_component/Identifiable.java
@@ -0,0 +1,9 @@
+package ua.kpi.dalgorithm.logic_component;
+
+/**
+ * Created on 05.04.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+public interface Identifiable extends Indexable, Nameable {
+}
---
+++ src/main/java/ua/kpi/dalgorithm/logic_component/Nameable.java
@@ -0,0 +1,12 @@
+package ua.kpi.dalgorithm.logic_component;
+
+/**
+ * Created on 05.04.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+public interface Nameable {
+    String getName();
+
+    void setName(String name);
+}
